fb6262628e7643a57f20661db5a48ca396aa996a
Kelvin <kelvinfamaral@hotmail.com>
2024-12-19 16:04:29 -0300

    Test

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+public class Program {
+    public static void main(String[] args) {
+
+
+        //Java Activity:
+        //Suppose a bank business that has a regular account and a business account.
+        //The business account includes all the features of the regular account, plus a loan limit and an operation to take out loans.
+
+        System.out.println("Test!");
+    }
+}
